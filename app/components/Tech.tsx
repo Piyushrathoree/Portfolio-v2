d47062eb7732baae06cc505edab764c292a72f28
Piyushrathoree <meonpath008@gmail.com>
2025-11-19 05:00:39 +0530

    feat: enhance Home page with personalized introduction and update Tech component usage

--- app/components/Tech.tsx
+++ app/components/Tech.tsx
@@ -4,10 +4,11 @@ import React from "react";
 interface TechProps {
   logo: string;
   name: string;
+  className?: string;
 }
 const Tech = (props: TechProps) => {
   return (
-    <div className="flex gap-1 px-3 py-1.5 border border-dashed bg-neutral-800 items-center justify-center ">
+    <div className={`flex gap-1 px-3 py-1.5 border border-dashed bg-neutral-800 items-center justify-center ${props.className}`}>
       <img src={props.logo} alt={props.name} className="h-5 w-5 " />
       <p className="text-center text-sm font-semibold">{props.name}</p>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,29 @@ import Avatar from "./components/Avatar";
 import Graph from "./components/Graph";
 import Tech from "./components/Tech";
 
-
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen py-2 bg-neutral-950">
-    <Graph/>
-    <Avatar/>
-    <Tech logo="/icons/vercel.svg" name="Vercel"/>
+      <Graph />
+
+      <div className="flex gap-3">
+        <Avatar />
+
+        <div>
+          <span className="text-2xl font-serif font-black flex items-center ">
+            Hi , <span className="">I'm Piyush Rathore</span> , a Full Stack
+            Developer!
+          </span>
+
+          <p className="font-mono ">
+            {" "}
+            specializing in building robust and scalable web applications using
+            <br /><span className="flex items-center gap-3">
+            <span>typescript and python & deploying them on</span>{" "}
+            <Tech logo="/icons/vercel.svg" name="Vercel" className=" w-20" /></span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
