c9aa90dc98e30d1f8e033990de8ed32298bf8cf2
Piyushrathoree <meonpath008@gmail.com>
2026-01-13 19:46:21 +0530

    minor changes

--- components/Tech.tsx
+++ components/Tech.tsx
@@ -9,7 +9,7 @@ interface TechProps {
 const Tech = (props: TechProps) => {
   return (
     <div
-      className={`flex gap-1 px-3 py-1 border dark:border-white/60 border-dashed dark:bg-neutral-900 bg-neutral-200 items-center cursor-pointer justify-center text-black dark:text-white shadow-inner shadow-neutral-400 dark:shadow-neutral-600 ${props.className}`}
+      className={`flex gap-1 px-3 py-1 border dark:border-white/60 border-dashed dark:bg-neutral-900 bg-neutral-200 items-center cursor-pointer justify-center text-black dark:text-white shadow-inner shadow-neutral-400/50 dark:shadow-neutral-600 ${props.className}`}
     >
       <img src={props.logo} alt={props.name} className="h-5 w-5 " />
       <p className="text-center text-sm font-semibold">{props.name}</p>
