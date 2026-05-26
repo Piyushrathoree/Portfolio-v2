c8324cdc3341da17226b3bca37cf180ab01a4111
PIYUSH RATHORE <163632958+Piyushrathoree@users.noreply.github.com>
2026-05-26 21:49:48 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -92,17 +92,11 @@ export default function RootLayout({
         <SpeedInsights />
       </body>
       <Script
-        src="https://migrate-supabase-into-neon-raah-beacon.studioone-tech.workers.dev/script.js"
-        data-pid="proj_ziebpffp39tz5ye3"
-        data-domain="piyushh.me"
-      />
-<script async src="https://plausible.io/js/pa-dp2pI7yKqLB8hFo4ZbplR.js"></script>
-<script>
-  window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
-  plausible.init()
-</script>
-
-      
-    </html>
+  src="https://t.raah.dev/script.js"
+  data-pid="proj_xb5gjwvb5tvcgm9j"
+  data-domain="google.com"
+  strategy="afterInteractive"
+/>
+  </html>
   );
 }
