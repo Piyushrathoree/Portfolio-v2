2c2fb2ac8559560d00642b2293eed1bd2c8a3769
PIYUSH RATHORE <163632958+Piyushrathoree@users.noreply.github.com>
2026-09-12 21:32:08 +0530

    Update profile.ts

--- data/profile.ts
+++ data/profile.ts
@@ -5,7 +5,7 @@ export const PROFILE = {
   timeZone: "Asia/Kolkata",
   email: "01piyush008@gmail.com",
   resumeUrl:
-    "https://drive.google.com/file/d/1dPtMM-VTzsMdXtuVnzFqVsrMsL-nnHoi/view?usp=sharing",
+    "https://drive.google.com/file/d/1s64i0UvX64aXXNlsWtGPWNTb4iGh9PlS/view?usp=sharing",
   calUrl: "https://cal.com/piyush-nkthix/15min",
   github: "Piyushrathoree",
   discordId: "1187126125722353768",
